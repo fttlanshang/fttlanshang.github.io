4619aa24dc13de6243a4bbceda81abdb69698844
fttlanshang <fttlanshang@gmail.com>
2020-11-11 18:22:43 -0500

    Site updated: 2020-11-11 18:22:42

--- js/custom.js
+++ js/custom.js
@@ -13,4 +13,22 @@ $(function () {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
+
+    var database = firebase.database();
+    var oriUrl = window.location.host;
+    var curUrl = oriUrl + window.location.pathname;
+    function readVisits(url, selector) {
+    var db_key = decodeURI(url.replace(new RegExp('\\/|\\.', 'g'), "_"));
+        database.ref(db_key).once("value").then(function (result) {
+            var count = parseInt(result.val() || 0) + 1;
+            database.ref(db_key).set(count);
+            if (selector.length > 0) {
+                selector.html(count);
+            };
+        });
+    }
+    readVisits(oriUrl, $("#visits .count"));
+    if (curUrl && curUrl != "_") {
+        readVisits("page/" + curUrl, $("#pageviews .count"));
+    }
 });
